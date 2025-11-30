64b05eb063d9478388971973e44770e30561fbbe
Kommiri <amaranath.kommiri@accenture.com>
2025-11-30 23:50:33 +0530

    terrform

---
+++ ec2.tf
@@ -0,0 +1,32 @@
+resource "aws_instance" "terraform" {
+  ami                     = "ami-09c813fb71547fc4f"
+  instance_type           = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
+
+  tags = {
+    Name = "terraform"
+    Terraform = "true"
+  }
+}
+
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+
+  egress {
+    from_port        = 0 # all ports are allowed
+    to_port          = 0
+    protocol         = "-1" # all protocols
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+    ingress {
+        from_port        = 0 # all ports are allowed
+        to_port          = 0
+        protocol         = "-1" # all protocols
+        cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name="allow-all"
+  }
+
+}
